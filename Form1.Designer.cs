2069fe3a77f816342a09577847744ba2022042c2
Пётр Мошкин <pidoridbrnjhjdyf@gmail.com>
2023-07-27 19:10:51 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace XML_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(412, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(786, 452);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 470);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(208, 64);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(394, 452);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(226, 470);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(180, 64);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(412, 470);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(159, 64);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create XML file";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 540);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+
+namespace XML_test
+{
+    public partial class Form1 : Form
+    {
+        private static string fileName = "Files\\24_21_1003001_2017-05-29_kpt11.xml";
+        private static string savedFileName = null;
+
+        private XDocument doc = XDocument.Load(fileName);
+                
+        private List<string> name = new List<string>() { "Parcels", "ObjectRealty (build)", "ObjectRealty (construction)", "SpatialData", "Bound", "Zone" };
+        private List<string> parent = new List<string>() { "land_record", "build_record", "construction_record", "spatial_data", "municipal_boundary_record", "zones_and_territories_record" };
+        private List<string> child = new List<string>() { "common_data", "common_data", "common_data", "entity_spatial", "b_object", "b_object" };
+        private List<string> id = new List<string> { "cad_number", "cad_number", "cad_number", "sk_id", "reg_numb_border", "reg_numb_border" };
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                treeView1.Nodes.Add(new TreeNode(name[i]));
+
+                Search(i, parent[i], child[i], id[i]);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string test;
+            if (e.Node.Parent != null)
+            {
+                savedFileName += e.Node.Parent.Text;
+                int numb = name.IndexOf(e.Node.Parent.Text);
+
+                foreach (XElement ele in doc.Descendants(parent[numb]))
+                {
+                    foreach (XElement ele2 in ele.Descendants(child[numb]))
+                    {
+                        test = ele2.Element(id[numb]).Value;
+                        if (test == e.Node.Text)
+                        {
+                            richTextBox1.Text +=  ele.ToString() + "\n\n";
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            savedFileName = null;
+        }
+
+        private void Search(int i, String name, String name2, String name3)
+        {
+            foreach (XElement ele in doc.Descendants(name))
+            {
+                foreach (XElement ele2 in ele.Descendants(name2))
+                {
+                    treeView1.Nodes[i].Nodes.Add((ele2.Element(name3).Value));
+                }
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string path = $@"Files\\XML_{savedFileName}.xml";
+                MessageBox.Show(path);
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + "<saved_information>\r\n" + richTextBox1.Text + "</saved_information>\r\n");
+
+                    fs.Write(info, 0, info.Length);
+                } 
+
+                richTextBox1.Clear();
+                savedFileName= null;               
+            }
+            
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
